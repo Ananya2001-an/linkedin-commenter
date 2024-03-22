03b8c67afd373e75c17e52869954cddb89b6f951
Ananya Nayak <ananyanayak102@gmail.com>
2024-03-23 01:39:38 +0530

    add get comments method

--- linkedin.py
+++ linkedin.py
@@ -1,53 +1,56 @@
 from linkedin_api.clients.restli.client import RestliClient
+from linkedin_api.clients.restli.response import CreateResponse, GetResponse
 
 
+"""
+LinkedInClient class for interacting with LinkedIn API
+"""
 class LinkedInClient:
-    def __init__(self, access_token):
-        self.access_token = access_token
-        self.restli_client = RestliClient()
+    restli_client: RestliClient = RestliClient()
 
-    def get_user_info(self):
-        response = self.restli_client.get(
+    def __init__(self, access_token):
+        self.access_token: str = access_token
+
+    def get_user_info(self) -> GetResponse:
+        return self.restli_client.get(
             resource_path="/userinfo",
             access_token=self.access_token
         )
-        return response.entity
     
-    # gives 403 forbidden
-    def get_comments_on_post(self, postId):
-        response = self.restli_client.get(
+    # works for "r_organization_social" oauth scope access tokens only
+    # restricted access to select devs with "r_member_social" scope
+    # need access to Advertising API endpoints; verify company page
+    def get_comments_on_post(self, postId) -> GetResponse:
+        return self.restli_client.get_all(
             resource_path="/socialActions/{actionUrn}/comments",
             path_keys={"actionUrn":f"urn:li:activity:{postId}"},
             access_token=self.access_token
         )
-        return response.entity
 
-    def comment_on_post(self, postId, userId):
-        response = self.restli_client.create(
+    def comment_on_post(self, postId, userId, message) -> CreateResponse:
+        return self.restli_client.create(
             resource_path="/socialActions/{actionUrn}/comments",
             path_keys={"actionUrn":f"urn:li:activity:{postId}"},
             entity={
-                "actor":f"urn:li:person:{userId}", # authenticated user urn (comment author urn)
+                "actor":f"urn:li:person:{userId}", # replace person with organization to comment as company
                 "message":{
-                    "text":"hello"
+                    "text":message
                 },
             },
             access_token=self.access_token
         )
-        return response.entity
     
 
-    def reply_to_comment(self, postId, commentId, userId):
-        response = self.restli_client.create(
+    def reply_to_comment(self, postId, commentId, userId, message) -> CreateResponse:
+        return self.restli_client.create(
             resource_path="/socialActions/{actionUrn}/comments",
             path_keys={"actionUrn":f"urn:li:comment:(urn:li:activity:{postId},{commentId})"},
             entity={
                 "actor":f"urn:li:person:{userId}",
                 "message":{
-                    "text":"replying to u"
+                    "text":message
                 },
                 "parentComment": f"urn:li:comment:(urn:li:activity:{postId},{commentId})",
             },
             access_token=self.access_token
         )
-        return response.entity
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from linkedin import LinkedInClient
+from linkedin_api.clients.restli.response import CreateResponse, GetResponse
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# example post and comment ids for testing
+# example postId for testing
 postId = "7148338267027935233"
-commentId = "7176868771452641280"
 
 
 def main():
     client = LinkedInClient(os.getenv("ACCESS_TOKEN"))
+    
     # user id of the authenticated user for creating new comments
-    userId = client.get_user_info()['sub']
-    # reply to a comment on a post
-    print(client.reply_to_comment(postId, commentId, userId))
+    user: GetResponse = client.get_user_info()
+    if(user.status_code == 407):
+        print("Failed to get user info")
+        return
+    userId: str = user['sub']
+
+    comments_response: GetResponse = client.get_comments_on_post(postId)
+    for comment in comments_response.entity['elements']:
+        commentId: str = comment['id']
+
+        # reply to a comment on a post
+        res: CreateResponse = client.reply_to_comment(postId, commentId, userId, "replying to a comment")
+        if res.status_code == 201:
+            print("Successfully replied to a comment")
+            # print(res.entity)
+        else:
+            print("Failed to reply to a comment")
 
 
 if __name__ == "__main__":
